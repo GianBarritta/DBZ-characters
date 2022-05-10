d928acb134b1bb1e5fb892481df1a324047ca9e0
Gian Barritta <gianbdeveloper@hotmail.com>
2022-05-10 19:42:16 -0300

    Repository is uploaded

---
+++ src/main/java/com/dbzCharacters/demo/repository/CharacterRepository.java
@@ -0,0 +1,12 @@
+package com.dbzCharacters.demo.repository;
+
+import com.dbzCharacters.demo.model.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository <Character, Long> {
+
+
+
+}
